7961e04ca7f11160b95eb19c56decdd0dd7cd007
Hara Chung <harachung90@gmail.com>
2023-02-26 19:36:08 +1100

    Second commit. Items; Looping through items

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 
 from flask import (Flask, render_template, redirect,
                    url_for, request, make_response)
+from options import DEFAULTS
 
 app = Flask(__name__)
 
@@ -15,13 +16,16 @@ def get_saved_data():
 
 @app.route('/')
 def index():  # put application's code here
-    data = get_saved_data()
-    return render_template('index.html', saves=data)
+    return render_template('index.html', saves=get_saved_data())
 
+
+@app.route('/builder')
+def builder():
+    return render_template('builder.html', saves=get_saved_data(), options=DEFAULTS)
 
 @app.route('/save', methods=['POST'])
 def save():
-    response = make_response(redirect(url_for('index')))
+    response = make_response(redirect(url_for('builder')))
     data = get_saved_data()
     data.update(dict(request.form.items()))
     response.set_cookie('character', json.dumps(data))
